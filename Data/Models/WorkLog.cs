11ce9a9e68528b67bca425497a9f57ea2a476713
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2025-01-19 18:07:54 +0300

    feat(worklogs): made basic CRUD for worklogs

--- Data/Models/WorkLog.cs
+++ Data/Models/WorkLog.cs
@@ -1,10 +1,16 @@
-﻿namespace DeploymentManagementSystem.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeploymentManagementSystem.Data.Models
 {
     public class WorkLog
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Not a valid time spent")]
         public decimal TimeSpent { get; set; }
+
         public int TaskId { get; set; }
         public string UserId { get; set; }
 
