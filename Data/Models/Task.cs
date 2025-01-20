c012ec48ddbfe78f1d510de588d2b318c8cd6de7
Sergei Tsvetkov <czwetkow.serega@yandex.ru>
2025-01-20 03:11:50 +0300

    feat(translation): Data directory

--- Data/Models/Task.cs
+++ Data/Models/Task.cs
@@ -3,23 +3,23 @@ using DeploymentManagementSystem.Data.DataAnnotations;
 
 namespace DeploymentManagementSystem.Data.Models
 {
-    [EndDateAfterStartDate(startDatePropertyName: "CreationDate", endDatePropertyName: "DueDate", ErrorMessage = "Due date must be later than creation date")]
+    [EndDateAfterStartDate(startDatePropertyName: "CreationDate", endDatePropertyName: "DueDate", ErrorMessage = "Срок выполнения должен быть позже даты создания")]
     public class Task
     {
-        public int Id {  get; set; }
+        public int Id { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "Maximum 100 characters allowed")]
+        [StringLength(100, ErrorMessage = "Максимум 100 символов")]
         public string Name { get; set; }
 
-        [StringLength(500, MinimumLength = 6, ErrorMessage = "Description, if specified, should be from 6 to 500 symbols")]
+        [StringLength(500, MinimumLength = 6, ErrorMessage = "Описание, если указано, должно быть от 6 до 500 символов")]
         public string? Description { get; set; }
 
         [Required]
         public DateTime CreationDate { get; set; }
         public DateTime? DueDate { get; set; }
 
-        public int TaskStatusId {  get; set; }
+        public int TaskStatusId { get; set; }
 
         [Required]
         public int? TaskTypeId { get; set; }
